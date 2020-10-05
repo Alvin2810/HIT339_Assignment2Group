c8658537b009fdb5e720b82f84af047cba150e8e
Alvin Monsi Mammoottil <S290871@students.cdu.edu.au>
2020-10-06 09:22:53 +0930

    Make changes to home

--- Assignment_2/Controllers/HomeController.cs
+++ Assignment_2/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using System.Web;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
@@ -6,32 +7,69 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Assignment_2.Models;
+using Assignment_2.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Dynamic;
+using Assignment_2.Viewmodel;
 
-namespace Assignment_2.Controllers
+namespace Assignment1.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ApplicationDbContext context)
         {
-            _logger = logger;
+            _context = context;
+        }
+        public ActionResult Index()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                List<ScheduleViewmodel> CustomerVMlist = new List<ScheduleViewmodel>(); // to hold list of Customer and order details
+
+
+                var schedulelist = (from bb in _context.MemberEnrol join aa in _context.Schedules on bb.ScheduleId equals aa.Id join cc in _context.Users on aa.Coach equals cc.Email select new { Date = aa.Date, CoachName = cc.Fname, Coach = cc.Id, Time = aa.Time, Location = aa.Location, Day = aa.Day, Member = bb.Member, Id = aa.Id }).ToList();
+
+                //query getting data from database from joining two tables and storing data in customerlist
+
+                foreach (var item in schedulelist)
+
+                {
+
+                    ScheduleViewmodel objcvm = new ScheduleViewmodel(); // ViewModel
+
+                    objcvm.Id = item.Id;
+
+                    objcvm.Date = item.Date;
+
+                    objcvm.Time = item.Time;
+
+                    objcvm.Coach = item.Coach;
+
+                    objcvm.CoachName = item.CoachName;
+
+                    objcvm.Day = item.Day;
+
+                    objcvm.Member = item.Member;
+
+                    objcvm.Location = item.Location;
+
+                    CustomerVMlist.Add(objcvm);
+
+                }
+
+                //Using foreach loop fill data from custmerlist to List<CustomerVM>.
+
+                return View(CustomerVMlist); //List of CustomerVM (ViewModel)
+            }
+            else
+            {
+                return Redirect("../Identity/Account/Login");
+            }
+
         }
 
-        public IActionResult Index()
-        {
-            return View();
-        }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
 }
