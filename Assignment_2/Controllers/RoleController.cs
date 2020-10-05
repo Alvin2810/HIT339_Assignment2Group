5e36cdef1885938a520a005ac3febfae9cccfbc8
RaymondTran301064 <61952938+RaymondTran301064@users.noreply.github.com>
2020-10-05 09:53:58 +0930

    Update RoleController.cs

--- Assignment_2/Controllers/RoleController.cs
+++ Assignment_2/Controllers/RoleController.cs
@@ -14,3 +14,23 @@ namespace Assignment_2.Controllers
         {
             this.roleManager = roleManager;
         }
+         [Authorize(Policy = "readpolicy")]
+        public IActionResult Index()
+        {
+            var roles = roleManager.Roles.ToList();
+            return View(roles);
+        }
+        [Authorize(Policy = "writepolicy")]
+        public IActionResult Create()
+        {
+            return View(new IdentityRole());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(IdentityRole role)
+        {
+            await roleManager.CreateAsync(role);
+            return RedirectToAction("Index");
+        }
+    }
+}
